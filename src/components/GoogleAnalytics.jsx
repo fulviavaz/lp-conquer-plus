1c1cad76ae725524e2da0a39e304235733309b2f
Fulvia Vaz <fulviavaz.dev@gmail.com>
2025-02-10 16:02:44 -0300

    feat: add GA Tags

---
+++ src/components/GoogleAnalytics.jsx
@@ -0,0 +1,43 @@
+"use client";
+
+import { useEffect } from "react";
+import Script from "next/script";
+import { usePathname } from "next/navigation";
+
+const GA_TRACKING_ID = "G-H51S6G6PYW"; // Substitua pelo seu ID
+
+export default function GoogleAnalytics() {
+  const pathname = usePathname();
+
+  useEffect(() => {
+    if (typeof window !== "undefined" && window.gtag) {
+      window.gtag("config", GA_TRACKING_ID, {
+        page_path: pathname,
+      });
+    }
+  }, [pathname]);
+
+  return (
+    <>
+      {/* Script do Google Analytics */}
+      <Script
+        strategy="afterInteractive"
+        src={`https://www.googletagmanager.com/gtag/js?id=${GA_TRACKING_ID}`}
+      />
+      <Script
+        id="google-analytics"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', '${GA_TRACKING_ID}', {
+              page_path: window.location.pathname,
+            });
+          `,
+        }}
+      />
+    </>
+  );
+}
